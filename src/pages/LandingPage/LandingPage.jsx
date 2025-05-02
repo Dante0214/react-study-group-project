351c0b7410801576fcee86500e505ae8bd46b4be
Sara han <hans63237@gmail.com>
2025-05-03 01:56:30 +0900

    style: 랜딩페이지 디자인변경

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./LandingPage.style.css";
 import { Box, Button, Typography, Grid, Container } from "@mui/material";
-import bannerImg from "./images/landing-page-book.png";
+// import bannerImg from "./images/landing-page-book.png";
 import mainPageImg from "./images/main-page-img.png";
 import vocaPageImg from "./images/voca-page-img.png";
 import { useNavigate } from "react-router-dom";
@@ -11,27 +11,32 @@ import StarIcon from "@mui/icons-material/Star";
 import CheckCircleIcon from "@mui/icons-material/CheckCircle";
 import { Dialog, DialogContent } from "@mui/material";
 import ScrollToTopButton from "../../common/components/Buttons/ScrollToTopButton";
+import logo from "../../../public/ddalkkak.ico";
+import logoWhite from "./images/logo-white.png";
+import iphoneMain from "./images/white-iphone.png";
 
 const cardData = [
   {
     title: "습관이 되는 영어",
     description:
-      "매일 관심 있는 영어 뉴스를 통해 자연스럽게 공부하는 루틴을 만드세요.",
-    color: "var(--color-primary-light)",
+      "매일 관심있는 영어 뉴스를 통해 습관처럼 공부하는 루틴을 만드세요.",
+    color: "var(--color-text-primary)",
+    // color: "var(--color-primary-light)",
     icon: <InfoIcon sx={{ color: "#fff", fontSize: 30 }} />,
   },
   {
     title: "AI 단어 정리",
-    description:
-      "AI가 기사 속 단어와 숙어를 자동으로 추출해 단어장에 정리해줘요.",
-    color: "var(--color-primary)",
+    description: "AI가 기사 속 단어들을 자동으로 추출해 리스트로 정리해줘요.",
+    color: "var(--color-text-secondary)",
+    // color: "var(--color-primary)",
     icon: <StarIcon sx={{ color: "#fff", fontSize: 30 }} />,
   },
   {
     title: "손 안의 단어장",
     description:
       "내가 저장한 단어와 예문을 한눈에! 언제 어디서든 복습할 수 있어요.",
-    color: "var(--color-primary-dark)",
+    color: "var(--color-text-disabled)",
+    // color: "var(--color-primary-dark)",
     icon: <CheckCircleIcon sx={{ color: "#fff", fontSize: 30 }} />,
   },
 ];
@@ -49,7 +54,7 @@ const LandingPage = () => {
     <div className="landing-body">
       <ScrollToTopButton />
       <Container
-        maxWidth="md"
+        maxWidth="full"
         disableGutters
         sx={{
           height: "100%",
@@ -57,22 +62,31 @@ const LandingPage = () => {
           display: "flex",
           flexDirection: "column",
           alignSelf: "center",
-          borderRadius: 4,
-          boxShadow: 3,
+          // borderRadius: 4,
+          // boxShadow: 3,
           p: 0,
-          marginTop: 4,
+          // marginTop: 2,
           position: "relative",
         }}
       >
-        <Box sx={{ position: "absolute", top: 0, left: 25 }}>
+        <Box
+          sx={{
+            position: "absolute",
+            top: 0,
+            right: 25,
+            display: { xs: "none", sm: "block" },
+          }}
+        >
           <Button
             variant="contained"
             onClick={handleStartClick}
             sx={{
               borderTopLeftRadius: 0,
               borderTopRightRadius: 0,
-              bgcolor: "var(--color-primary)",
-              color: "var(--color-text-primary)",
+              bgcolor: "#fff",
+              color: "var(--color-primary)",
+              // bgcolor: "var(--color-primary)",
+              // color: "var(--color-text-primary)",
               fontWeight: "bold",
               zIndex: 10,
               transition: "all 0.3s ease-in-out",
@@ -85,11 +99,35 @@ const LandingPage = () => {
             Get started
           </Button>
         </Box>
+        <Box
+          sx={{
+            width: "100%",
+            height: "80px",
+            bgcolor: "var(--color-text-primary)",
+            textAlign: "center",
+          }}
+        >
+          <Grid
+            container
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <img src={`${logo}`} alt="파비콘로고" srcset="" width={80} />
+            <Typography variant="body1" color="#fff" fontWeight="bold">
+              AI와 함께 매일 관심 있는 영어 뉴스를 통해 자연스럽게 공부하는 루틴
+            </Typography>
+          </Grid>
+        </Box>
 
         <Box
           sx={{
-            flex: "6 1 0%",
-            backgroundImage: `url(${bannerImg})`,
+            flex: "5 1 0%",
+            minHeight: { xs: "380px", md: "500px" },
+            backgroundColor: "var(--color-primary)",
+            // backgroundImage: `url(${bannerImg})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
             backgroundRepeat: "no-repeat",
@@ -98,39 +136,101 @@ const LandingPage = () => {
             justifyContent: "center",
             alignItems: "center",
             p: 4,
-            borderTopLeftRadius: 16,
-            borderTopRightRadius: 16,
+            // borderTopLeftRadius: 16,
+            // borderTopRightRadius: 16,
           }}
         >
+          {/* 버전 2 배너 영역 */}
           <Grid
             container
-            spacing={3}
-            sx={{
-              zIndex: 2,
-              justifyContent: "center",
-              pt: { xs: 4, md: 0 },
-            }}
+            sx={{ width: { xs: "100%", md: "60%" } }}
+            display="flex"
+            justifyContent="center"
           >
-            {cardData.map((card, index) => (
-              <Grid size={{ xs: 12, sm: 4 }} key={index}>
-                <LandingCardItem
-                  title={card.title}
-                  description={card.description}
-                  color={card.color}
-                  icon={card.icon}
-                />
-              </Grid>
-            ))}
+            <Grid
+              size={{ md: 6, sm: 12 }}
+              sx={{ display: "flex", justifyContent: "center" }}
+            >
+              <Box textAlign="left">
+                <Typography variant="body1" color="#Fff" fontWeight="bold">
+                  현명한 사람은 꼭 사용하는 AI
+                </Typography>
+                <Typography
+                  variant="body1"
+                  color="#Fff"
+                  fontWeight="bolder"
+                  fontSize="2em"
+                >
+                  대한민국 1위
+                </Typography>
+                <Typography
+                  variant="body1"
+                  color="#Fff"
+                  fontWeight="bolder"
+                  sx={{
+                    fontSize: { xs: "1.5em", md: "2em" },
+                    whiteSpace: "nowrap",
+                  }}
+                >
+                  영단어 마스터 플랫폼
+                </Typography>
+                <Box
+                  sx={{ display: "flex", alignItems: "center", marginTop: 2 }}
+                >
+                  <img
+                    src={`${logoWhite}`}
+                    alt="파비콘화이트로고"
+                    srcset=""
+                    width={60}
+                    color="#fff"
+                  />
+                  <Typography
+                    variant="body1"
+                    color="#Fff"
+                    fontWeight="bolder"
+                    fontSize="2.5em"
+                    marginLeft="5px"
+                  >
+                    딸깍영어
+                  </Typography>
+                </Box>
+              </Box>
+            </Grid>
+            {/*아이폰 목업 영역 */}
+            <Grid
+              size={{ md: 6 }}
+              sx={{
+                display: { xs: "none", md: "flex" },
+                justifyContent: "flex-start",
+                position: "relative",
+                minHeight: "100%",
+              }}
+            >
+              <Box
+                sx={{
+                  position: "absolute",
+                  top: { md: "150px" },
+                  cursor: "pointer",
+                  transition: "all 0.4s ease-in-out",
+                  "&:hover": {
+                    top: { md: "-90px" },
+                    transform: "scale(1.05)",
+                  },
+                }}
+              >
+                <img src={`${iphoneMain}`} alt="아이폰목업" width="400px" />
+              </Box>
+            </Grid>
           </Grid>
-          ;
         </Box>
+
         <Box
           sx={{
-            flex: "4 1 0%",
+            flex: "5 1 0%",
             width: "100%",
             // bgcolor: "var(--color-primary)",
             display: "flex",
-            flexDirection: { xs: "column", md: "row" },
+            flexDirection: "column",
             justifyContent: "center",
             alignItems: "center",
             borderBottomLeftRadius: 16,
@@ -138,66 +238,96 @@ const LandingPage = () => {
             gap: 3,
             px: 4,
             py: 3,
+            position: "relative",
+            zIndex: "1005",
+            bgcolor: "#fff",
           }}
         >
-          {/* 메인 페이지 이미지 */}
-          <Box sx={{ textAlign: "center" }}>
-            <Box
-              component="img"
-              src={mainPageImg}
-              alt="메인페이지"
-              onClick={() => setOpenImage(mainPageImg)}
+          {/*버전1 카드 데이터 */}
+          <Box sx={{ marginTop: { xs: 0, md: 2 } }}>
+            <Grid
+              container
+              spacing={3}
               sx={{
-                cursor: "pointer",
-                width: { xs: "100%", sm: 300 },
-                height: 200,
-                borderRadius: 3,
-                boxShadow: 3,
-                mb: 1,
-                transition: "all 0.3s ease-in-out",
-                "&:hover": {
-                  transform: "scale(1.05)",
-                  boxShadow: 6,
-                },
+                zIndex: 2,
+                justifyContent: "center",
+                pt: { xs: 4, md: 0 },
               }}
-            />
-            <Typography
-              variant="body2"
-              color="var(--color-primary)"
-              fontWeight="bold"
             >
-              뉴스 기반 학습 페이지
-            </Typography>
+              {cardData.map((card, index) => (
+                <Grid size={{ xs: 12, sm: 4 }} key={index}>
+                  <LandingCardItem
+                    title={card.title}
+                    description={card.description}
+                    color={card.color}
+                    icon={card.icon}
+                  />
+                </Grid>
+              ))}
+            </Grid>
           </Box>
+          <Box sx={{ display: { xs: "block", md: "flex" }, gap: 3 }}>
+            {/* 메인 페이지 이미지 */}
+            <Box sx={{ textAlign: "center", my: 1 }}>
+              <Box
+                component="img"
+                src={mainPageImg}
+                alt="메인페이지"
+                onClick={() => setOpenImage(mainPageImg)}
+                sx={{
+                  cursor: "pointer",
+                  width: { xs: "100%", sm: 300 },
+                  height: 200,
+                  borderRadius: 3,
+                  boxShadow: 3,
+                  mb: 1,
+                  transition: "all 0.3s ease-in-out",
+                  "&:hover": {
+                    transform: "scale(1.05)",
+                    boxShadow: 6,
+                  },
+                }}
+              />
+              <Typography
+                variant="body2"
+                color="var(--color-primary)"
+                fontWeight="bold"
+                fontSize="1em"
+              >
+                뉴스 기반 학습 페이지
+              </Typography>
+            </Box>
 
-          {/* 단어장 페이지 이미지 */}
-          <Box sx={{ textAlign: "center" }}>
-            <Box
-              component="img"
-              src={vocaPageImg}
-              alt="단어장페이지"
-              onClick={() => setOpenImage(vocaPageImg)}
-              sx={{
-                cursor: "pointer",
-                width: { xs: "100%", sm: 300 },
-                height: 200,
-                borderRadius: 3,
-                boxShadow: 3,
-                mb: 1,
-                transition: "all 0.3s ease-in-out",
-                "&:hover": {
-                  transform: "scale(1.05)",
-                  boxShadow: 6,
-                },
-              }}
-            />
-            <Typography
-              variant="body2"
-              color="var(--color-primary)"
-              fontWeight="bold"
-            >
-              단어장 기능 미리보기
-            </Typography>
+            {/* 단어장 페이지 이미지 */}
+            <Box sx={{ textAlign: "center", my: 1 }}>
+              <Box
+                component="img"
+                src={vocaPageImg}
+                alt="단어장페이지"
+                onClick={() => setOpenImage(vocaPageImg)}
+                sx={{
+                  cursor: "pointer",
+                  width: { xs: "100%", sm: 300 },
+                  height: 200,
+                  borderRadius: 3,
+                  boxShadow: 3,
+                  mb: 1,
+                  transition: "all 0.3s ease-in-out",
+                  "&:hover": {
+                    transform: "scale(1.05)",
+                    boxShadow: 6,
+                  },
+                }}
+              />
+              <Typography
+                variant="body2"
+                color="var(--color-primary)"
+                fontWeight="bold"
+                fontSize="1em"
+              >
+                단어장 기능 미리보기
+              </Typography>
+            </Box>
           </Box>
         </Box>
         {/* 미리보기 모달 */}
